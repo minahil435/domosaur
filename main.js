2ef3cfca41014c7de366b0ea6a28cf04d378bd39
Minahil shafique <minahal.shafique@gmail.com>
2021-03-16 12:53:02 -0400

    functionclicks

--- main.js
+++ main.js
@@ -1,41 +1,48 @@
 
-const messWithMe = documents.querySelector('.mess-with-me');
-messWithMe.style.fontSize = '40px';
-messWithMe.style.backgroundColor = 'green';
+const messWithMe = document.querySelector('.mess-with-me');
+messWithMe.style.fontSize = '48px';
+
+const messWithMeP = document.querySelector('p.mess-with-me');
+messWithMeP.style.backgroundColor = 'green';
 messWithMe.addEventListener("click",spanClicked);
 
-const hideImg = documents.querySelector("#hide-me");
+const hideImg = document.querySelector("#hide-me");
 hideImg.style.display = 'none';
 
-const Img = documents.querySelector("#triceratops");
-Img.style.height = '324px';
-Img.addEventListener("click",ImgeClicked);
+const Imge = document.querySelector("#triceratops");
+Imge.style.width = '324px';
+Imge.addEventListener("click",ImgeClicked);
 
-const feathers = documents.querySelector("#feathers");
+const feathers = document.querySelector("#feathers");
 feathers.addEventListener("click",feathersTransparent);
 
-const button = documents.querySelector("#toggle");
+const button = document.querySelector("#toggle");
 button.addEventListener("click",buttonPressed);
 
-const bigImge = documents.querySelector("#biggify");
-bigImge.addEventListener("mouseOver",bigImage);
+const bigImge = document.querySelector("#biggify");
+bigImge.addEventListener("mouseenter",bigImage);
+bigImge.addEventListener("mouseleave",smallImage);
+
 
 
 function spanClicked(){
     messWithMe.style.color = 'orange';
 }
 function ImgeClicked(){
-    Img.style.border = '1px solid red';
+    Imge.style.border = '1px solid red';
 }
 function feathersTransparent(){
-    feathers.style.opacity ="0.5"
+    feathers.style.opacity = "0.5"
 }
 function buttonPressed(){
-    const rowDino = documents.querySelector("#row");
+    const rowDino = document.querySelector("#row");
     rowDino.style.backgroundColor = 'yellow';
 }
 function bigImage(){
     bigImge.style.width = '200px'
+}
+function smallImage(){
+    bigImge.style.width = '150px'
 }
 
 
